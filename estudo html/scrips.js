71480e0207156ea71b884fdfb254e4ad265b2ae1
msm1996 <master19041996@gmail.com>
2024-12-11 21:15:46 -0300

    Meu primeiro commit

---
+++ estudo html/scrips.js
@@ -0,0 +1,22 @@
+let imagem = document.querySelector(".starbucks")
+let circulo = document.querySelector(".circulo")
+
+console.log(imagem)
+
+function trocarimagem(endereco){
+    imagem.src = endereco
+
+}
+
+
+
+function trocarcor(cor){
+    circulo.style.background = cor
+}
+
+
+
+
+ 
+
+
